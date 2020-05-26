4edfb6d84f0397e2ebc78bb8b5da3c11b92d064c
xilen0x <carlos.astorgax@gmail.com>
2020-05-26 19:48:16 -0400

    ajustes

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -3,7 +3,7 @@ import React from 'react';
 function MyNavbar(props) {
     return (
         <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-            <a className="navbar-brand" href="#">Start Boostrap</a>
+            <a className="navbar-brand" href="#">LandingPage</a>
             <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
             </button>
